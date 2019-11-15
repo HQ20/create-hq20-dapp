4c4a0b6980822be3446d5c253c7eba5aa4a7caa0
Bernardo Vieira <bernardo5@live.com.pt>
2019-11-15 18:18:30 +0000

    fix ci and tests

--- workspace-webui/src/Pages/Main/__tests__/Main.test.tsx
+++ workspace-webui/src/Pages/Main/__tests__/Main.test.tsx
@@ -7,16 +7,20 @@ import * as React from 'react';
 // eslint-disable-next-line no-unused-vars
 import Main from '../Main';
 
+import { ethers } from 'ethers';
+import { JsonRpcProvider } from 'ethers/providers';
+import { SimpleStorageInstance } from 'workspace-blockchain/types/truffle-contracts';
+
 
 configure({ adapter: new Adapter() });
 /**
  * Define class interface
  */
 interface IMainState {
-    storageValue: number;
-    web3: any;
-    accounts: string[];
-    contract: any;
+    storageValue: string;
+    userSigner: ethers.providers.JsonRpcSigner;
+    provider: JsonRpcProvider;
+    simpleStorageInstance: ethers.Contract & SimpleStorageInstance;
 }
 describe('Basic tests to Main Component', () => {
     let mainWrapper: ReactWrapper<{}, IMainState>;
@@ -25,16 +29,16 @@ describe('Basic tests to Main Component', () => {
         mainWrapper = mount(<Main />);
     });
 
-    test('web3 gets defined', async () => {
+    test('provider gets defined', async () => {
         let leftTries = 5;
-        // wait until web3 is defined
+        // wait until provider is defined
         do {
             // eslint-disable-next-line no-await-in-loop
             await delay(3000);
             mainWrapper.update();
             leftTries -= 1;
-        } while (mainWrapper.state().web3 !== undefined && leftTries >= 0);
-        expect(mainWrapper.state().web3).not.toBe(undefined);
+        } while (mainWrapper.state().provider !== undefined && leftTries >= 0);
+        expect(mainWrapper.state().provider).not.toBe(undefined);
     }, 20000);
 
     test('Should list actions', async () => {
@@ -47,7 +51,7 @@ describe('Basic tests to Main Component', () => {
             await delay(3000);
             mainWrapper.update();
             leftTries -= 1;
-        } while (mainWrapper.state().storageValue === 5 && leftTries >= 0);
-        expect(mainWrapper.state().storageValue).toBe(5);
+        } while (mainWrapper.state().storageValue === '5' && leftTries >= 0);
+        expect(mainWrapper.state().storageValue).toBe('5');
     }, 20000);
 });
