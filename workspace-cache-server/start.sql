60d31b516ffbd7b0373ed02b55bd11e22377ff6b
Bernardo Vieira <bernardo5@live.com.pt>
2020-01-14 10:44:45 +0000

    fix typo

--- workspace-cache-server/start.sql
+++ workspace-cache-server/start.sql
@@ -1,2 +1,2 @@
 CREATE DATABASE starterkitcache;
-CREATE TABLE setvalue(id integer auto increment primary key, newvalue integer, tx text unique, sender text)
+CREATE TABLE setvalue(id integer serial primary key, newvalue integer, tx text unique, sender text)
